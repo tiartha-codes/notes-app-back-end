218f5dd4efa1520f4a9bcb62a7dc013e42f38a79
Tiartha Codes <igustiagungsugihartha@gmail.com>
2024-11-19 14:03:41 +0800

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+/* eslint-disable linebreak-style */
+const { addNoteHandler, getAllNotesHandler, getNoteByIdhandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdhandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+
+];
+
+module.exports = routes;
